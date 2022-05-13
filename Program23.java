c4c9853460ac982da872471d3baec8017fd2cec8
rahulshelke99 <105413847+rahulshelke99@users.noreply.github.com>
2022-05-13 23:30:21 +0530

    First commit

--- Program23.java
+++ Program23.java
@@ -1,18 +1,29 @@
+/**
+*imported a scanner package. 
+*/
 import java.util.Scanner;
 class ReverseNumber
 {
 	public static void main(String[] args) 
 	{
+		/**
+		*called scanner object to get user value.
+		**/
 		Scanner s = new Scanner(System.in);
+		/**
+		*The value should be integer type.
+		**/
 		System.out.println("Enter the number to be reversed: ");
 		int num = s.nextInt();
 		int ans = 0;
 		while (num>0)
 		{
-			int rem = num%10;
-			ans = ans*10+rem;
-			num /= 10;
+			int rem = num%10;	// extract the last number from given value.
+			ans = ans*10+rem;	//stored that value in variable call ans.
+			num /= 10;		//every type update the value dividing by 10. 
 		}
+		/**@return the reverese value of given number.
+		**/
 		System.out.println(ans);
 	}
 }
